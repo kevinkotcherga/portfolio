dbd6c3f41fb5b76f3c2834d4d445e4d3befe16dc
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-12 12:44:06 +0100

    ✨feat(meta-tags): ajout d'images et descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'MyMovies.dev',
+      title: 'MyMovies',
+      reverse: true,
+      separator: '|',
+      description: 'The most popular movie website in the world',
+      keywords: 'action, horror, drama',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('apple-touch-icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'MyMovies.dev',
+        title: 'MyMovies',
+        description: 'The most popular movie website in the world',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('login-page.png')
+      }
+    }
+  end
 end
