939b51c7e2870b1dbc6e0d6ddd8da897a1c07a83
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-21 11:51:11 +0100

    ✨feat(gemfile): gem pg

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,34 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-        const toggle = (body) => {
-      // si la valeur de toggled dans le localstorage est vide, alors on applique le dark theme
-      if (localStorage.toggled === '') {
-        body.classList.add('dark');
-        localStorage.toggled = "dark";
-        // si non on applique le light theme
-      } else {
-        body.classList.remove('dark');
-        localStorage.toggled = "";
-      }
+      const toggle = (body) => {
+    // si la valeur de toggled dans le localstorage est vide, alors on applique le dark theme
+    if (localStorage.toggled === '') {
+      body.classList.add('dark');
+      localStorage.toggled = "dark";
+      // si non on applique le light theme
+    } else {
+      body.classList.remove('dark');
+      localStorage.toggled = "";
+    }
+  }
+    const body = document.querySelector("body")
+    const checkbox = document.querySelector("#checkBox")
+
+    // au chargement de la page(changement de page sur le site) on checke (dans le local storage) si le dark theme est le theme choisi
+    if (localStorage.toggled === 'dark') {
+      // on applique la class au body
+      body.classList.add('dark');
+      checkbox.checked = true;
     }
 
-
-
-      const body = document.querySelector("body")
-      const checkbox = document.querySelector("#checkBox")
-
-      // au chargement de la page(changement de page sur le site) on checke (dans le local storage) si le dark theme est le theme choisi
-      if (localStorage.toggled === 'dark') {
-        // on applique la class au body
-        body.classList.add('dark');
-        checkbox.checked = true;
-      }
-
-      // on écoute le click sur la checkbox
-      checkbox.addEventListener("click", (event) => {
-        // on lance la fonction toggle, en lui passant le body
-        toggle(body)
-      })
+    // on écoute le click sur la checkbox
+    checkbox.addEventListener("click", (event) => {
+      // on lance la fonction toggle, en lui passant le body
+      toggle(body)
+    })
   }
 }
